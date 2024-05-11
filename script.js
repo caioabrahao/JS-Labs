059093005ba2accc5bb3c2b6184ad4c90c00dc0e
Caio Abrahão <contato.caioabrahao@gmail.com>
2024-05-11 16:13:06 -0300

    quiz semi-working

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ function experimentOneChangeText (){
                                 "Huh... Fine...",
                                 "Let's count! Bare with me!"]
 
-    if(messageIndex < 13){
+    if(messageIndex < textMessages.length){
         text.textContent = textMessages[messageIndex];
         messageIndex++;
     }
 
-    if(messageIndex >= 13){
+    if(messageIndex >= textMessages.length){
         let timesPressed = messageIndex.toString();
         text.innerHTML = "You clicked this stupid button <span class='text-highlight'>" + timesPressed + " </span>times!";
         messageIndex++;
@@ -29,18 +29,72 @@ function experimentOneChangeText (){
 
 }
 
-function quizDisplayUpdate (){
+let quizIndex = 0;
+const questions = [
+    {question: "Is it raining today?", answer: false},
+    {question: "Are you a big fan of hot dogs", answer: false},
+    {question: "67 is greater than 69", answer: false}
+];
 
+function quizDisplayUpdate (){
+        const quizDisplay = document.getElementById("quiz-display");
+        quizDisplay.textContent = questions[quizIndex].question;
 }
 
 function answerYes(){
-
+    questions[quizIndex].answer = true;
+    quizIndex++;
+    if(quizIndex < questions.length) {
+        quizDisplayUpdate();
+    } else {
+        endQuiz();
+    }
 }
 
 function answerNo(){
-
+    questions[quizIndex].answer = false;
+    quizIndex++;
+    if(quizIndex < questions.length) {
+        quizDisplayUpdate();
+    } else {
+        endQuiz();
+    }
 }
 
 function startQuiz(){
+    let yesButton = document.getElementById("yes-button");
+    let noButton = document.getElementById("no-button");
+    quizDisplayUpdate();
+    yesButton.removeAttribute("disabled");
+    noButton.removeAttribute("disabled");
     document.getElementById("start-quiz-button").remove();
+}
+
+function endQuiz(){
+    const quizDisplay = document.getElementById("quiz-display");
+    quizDisplay.textContent = "The quiz has come to an end! Wanna see the results?";
+    let yesButton = document.getElementById("yes-button");
+    let noButton = document.getElementById("no-button");
+    yesButton.remove();
+    noButton.remove();
+
+    let newButton = document.createElement("button");
+    newButton.textContent = "Sure! Lets see it.";
+    newButton.addEventListener("click", seeResults);
+    newButton.type = "button";
+    newButton.className = "button";
+
+    document.getElementById("quiz-buttons").appendChild(newButton);
+}
+
+function seeResults(){
+    const quizDisplay = document.getElementById("quiz-display");
+    quizDisplay.remove();
+
+    for(let i = 0; i < questions.length; i++){
+        let answerText = document.createElement("p");
+        let questionListNumber = i + 1;
+        answerText.innerHTML = questionListNumber.toString() + ". " + questions[i].question + " = " + "<span class='text-highlight'>" + questions[i].answer  + "</span>";
+        document.getElementById("quiz-text-box").appendChild(answerText);
+    }
 }
